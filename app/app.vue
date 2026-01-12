ca1a506eb605ae66e0a161f8f61f0441ce472a70
tanxgomut <tang@rush1auto.co>
2026-01-12 15:28:52 +0700

    update LMB

--- app/app.vue
+++ app/app.vue
@@ -13,7 +13,13 @@ useHead({
         lang: 'th'
     },
     link: [
-        { rel: 'icon', type: 'image/png', href: '/favicon.png' },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
         { rel: 'canonical', href: `${siteUrl}${route.path}` }
     ]
 })
